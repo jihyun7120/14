a9da6176ed470190e2610054feef790ed65dd8d2
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-12-07 21:58:18 +0900

    practice1

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    
+    int i = 0;
+    char *pc = NULL;
+    
+    pc = (char*)malloc(100*sizeof(char));
+    if (pc == NULL) 
+    {
+           printf("ERROR\n");
+           exit(1); 
+    }
+    
+    for (i=0;i<26;i++)
+        pc[i] = 'a' + i;//abcdefg..0
+    pc[i] = 0;//null 0
+    
+    printf("%s\n", pc);
+    
+    free(pc);
+     
+  
+  system("PAUSE");	
+  return 0;
+}
